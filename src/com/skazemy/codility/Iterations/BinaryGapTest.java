42bc5b382f454919c5207b29ca5f882583fbfe71
Sadegh Kazemy <sadegh.kazemy@gmail.com>
2017-09-23 11:54:36 +0330

    Refactor BinaryGap.

--- src/com/skazemy/codility/Iterations/BinaryGapTest.java
+++ src/com/skazemy/codility/Iterations/BinaryGapTest.java
@@ -1,18 +1,19 @@
 package com.skazemy.codility.Iterations;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+
 
 public class BinaryGapTest {
 
     @Test
     public void solution() throws Exception {
-        BinaryGap.Solution solution = new BinaryGap.Solution();
-        assertEquals(0, solution.solution(Integer.parseInt("10000", 2)));
-        assertEquals(4, solution.solution(Integer.parseInt("100001", 2)));
-        assertEquals(3, solution.solution(Integer.parseInt("100011111001101", 2)));
-        assertEquals(4, solution.solution(Integer.parseInt("1100001000000", 2)));
+        BinaryGap binaryGap = new BinaryGap();
+        assertEquals(0, binaryGap.solution(Integer.parseInt("10000", 2)));
+        assertEquals(4, binaryGap.solution(Integer.parseInt("100001", 2)));
+        assertEquals(3, binaryGap.solution(Integer.parseInt("100011111001101", 2)));
+        assertEquals(4, binaryGap.solution(Integer.parseInt("1100001000000", 2)));
 
     }
 
